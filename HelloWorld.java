8c9933f0b6a6254b02519af620ab91a03762e4c2
sagarkumarnsk <87412963+sagarkumarnsk@users.noreply.github.com>
2021-07-29 01:32:35 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
   public static void main(String[] args) {
       System.out.println("");
     System.out.println("");
-    System.out.println("Hello, World");
+    System.out.println("Hello, sneha");
     System.out.println("Hello, World");
     System.out.println("Hello, World");
     System.out.println("Hello, World");
